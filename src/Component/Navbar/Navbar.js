14d288f4f753a09566fbe48d217ca3087cf5226e
sanjeetsk <ssanjeetkumar67@gmail.com>
2024-04-08 23:57:37 +0530

    updated navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,19 +1,44 @@
-import { NavLink } from "react-router-dom"
-import './Navbar.css'
+
+import { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import './Navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <div id="navbar">
-            <NavLink to="https://drive.google.com/file/d/1fZrB4E2SK6oVJN9ln7uYIiH93o4xdYAL/view?usp=sharing">Resume</NavLink>
-            <div id="navlinks">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/project/">Projects</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <NavLink to="/" className="navbar-logo">Sanjeet Kumar</NavLink>
+                <button className={`navbar-toggler ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
             </div>
-        </div>
-    )
-}
+            <ul className={`navbar-nav ${isOpen ? 'active' : ''}`}>
+                <li className="nav-item">
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/projects">Projects</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/contact">Contact</NavLink>
+                </li>
+                <li className="nav-item">
+                    <a href="https://drive.google.com/file/d/1f4BYeDsweT3ZrPM93CbxfAAxX_x8zQQp/view?usp=sharing" className="resume-link" target="_blank" rel="noopener noreferrer">Resume</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
